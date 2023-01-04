56906ccee4324a5f4980cda05ae0663adfa80e98
Norsi47 <briggsnorsi@yahoo.com>
2023-01-03 23:00:57 -0500

    proper way to get random choice

--- Starting/RPS.js
+++ Starting/RPS.js
@@ -1,3 +1,12 @@
 //computers choice, random pick this when called
-const getComputerChoice = "Rock, Paper or Scissors";
-console.log(getComputerChoice);
+// const getComputerChoice = "Rock, Paper or Scissors";
+// console.log(getComputerChoice);
+
+function getComputerChoice() {
+  const randomPick = ['Rock', 'Paper', 'Scissors'];
+  return randomPick[Math.floor(Math.random() *randomPick.length) ]
+}
+console.log(getComputerChoice());
+
+
+//single round of rock paper scissors
