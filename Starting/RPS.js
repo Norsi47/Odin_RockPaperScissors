d2680bf823f5c6e7a9de501a84c472f5aaaf7ae6
Norsi47 <briggsnorsi@yahoo.com>
2023-01-22 22:11:56 -0500

    adding logic to print out score and say who wins

--- Starting/RPS.js
+++ Starting/RPS.js
@@ -8,11 +8,10 @@ function getComputerChoice() {
     return randomPick[Math.floor(Math.random() * randomPick.length)]
 }
 
-// console.log(getComputerChoice());
 
 //the score player starts with
-let playersStartScore = 10;
-let cpuStartScore = 10;
+let playersStartScore = 5;
+let cpuStartScore = 5;
 
 //single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
@@ -20,31 +19,24 @@ function playRound(playerSelection, computerSelection) {
     // computerSelection = getComputerChoice();
     if (playerSelection === "rock" && computerSelection === "paper") {
 
-        return `You Lose! Paper Beats Rock, Players Score = ${playersStartScore - 1}, CPU Score = ${cpuStartScore + 1}`;
+        return `You Lose! Paper Beats Rock, Players Score = ${playersStartScore - 1}, CPU Score = ${cpuStartScore++}`;
 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You win Scissors beats Paper, Players Score = ${playersStartScore + 1} , CPU Score = ${cpuStartScore - 1}`;
+        return `You win Scissors beats Paper, Players Score = ${playersStartScore + 1} , CPU Score = ${cpuStartScore--}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return `You Lose rock beats scissors, Players Score = ${playersStartScore - 1}, CPU Score = ${cpuStartScore +1}`;
+        return `You Lose rock beats scissors, Players Score = ${playersStartScore - 1}, CPU Score = ${cpuStartScore++}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
 
-        return `You Win Paper beats rock, Players Score = ${playersStartScore + 1}, CPU Score = ${cpuStartScore - 1}`;
+        return `You Win Paper beats rock, Players Score = ${playersStartScore + 1}, CPU Score = ${cpuStartScore--}`;
     } else if (playerSelection === "scissors" && computerSelection === `Rock`) {
-        return  `You Lose Rock beats Scissors. Players Score= ${playersStartScore -1}, CPU Score = ${cpuStartScore + 1}`;
+        return `You Lose Rock beats Scissors. Players Score= ${playersStartScore - 1}, CPU Score = ${cpuStartScore++}`;
 
-    }
-    else if (playerSelection === computerSelection) {
-        return `It is a tie you both picked ${playerSelection}, Player Score = ${playersStartScore}, CPU Score = ${cpuStartScore} no points added, it is a tie` ;
-     }
-    else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}, ComputerWins ${cpuStartScore + 1}, PlayerOne Wins ${playersStartScore -1}`;
+    } else if (playerSelection === computerSelection) {
+        return `It is a tie you both picked ${playerSelection}, Player Score = ${playersStartScore}, CPU Score = ${cpuStartScore} no points added, it is a tie`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}, ComputerWins ${cpuStartScore++}, PlayerOne Wins ${playersStartScore--}`;
     }
 
-
-    if (playersStartScore || cpuStartScore > 10) {
-        return `Hey You won the game`;
-
-    }
 
 }
 
@@ -55,17 +47,27 @@ function playRound(playerSelection, computerSelection) {
 * -> print out the winner from who ever got selected 5 times in the loop*/
 function game() {
 //will loop through prompt question 5 times, start from 1-5 not 0-5
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i < 6; i++) {
 //to test player selection vs computer selection
 //prompting user to enter solution
         const playerSelection = prompt("Hello, Please pick: Rock, Paper or Scissors").toLowerCase();
         const computerSelection = getComputerChoice();
-        //print out who won
-        console.log(playRound(playerSelection, computerSelection));
 
 //print out the players and cpu selection to uppercase
         console.log(`\n ${i}-> Player Picked ${playerSelection} and Computer chose ${computerSelection}`.toUpperCase());
 
+        //print out who won
+        console.log(playRound(playerSelection, computerSelection));
+
+    }
+
+//to print out who wins
+    if (cpuStartScore > 6) {
+        console.log("Computer wins");
+    } else if (playersStartScore > 6) {
+        console.log("Player wins")
+    } else {
+        return "Nobody won";
     }
 
 }
